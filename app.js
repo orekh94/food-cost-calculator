08a05ff35e9846230408e155f79fc4083c56fff0
orekh94 <denisov@orekh94.ru>
2025-02-05 13:31:44 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+{
+  ingredients: [
+    {
+      name: "Морковь",
+      category: "Овощи",
+      prices: [
+        {supplier: "ООО Поставщик1", price: 50},
+        {supplier: "ИП Иванов", price: 45}
+      ],
+      unit: "кг",
+      waste: 10
+    }
+  ],
+  suppliers: ["ООО Поставщик1", "ИП Иванов"],
+  categories: ["Овощи", "Мясо"...]
+}
